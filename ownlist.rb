913c69f531f39d9eca722b5cb0faa410e35187b3
imhilla <hillaryodhiambo282@gmail.com>
2022-10-04 19:49:05 +0300

    append value

--- ownlist.rb
+++ ownlist.rb
@@ -10,3 +10,44 @@
 # 2. append_after
 # 3. delete
 # 4. find
+class Node
+  attr_accessor :next
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+  def to_s
+    "Node with value:#{@value}"
+  end
+end
+
+class LinkedList
+  def initialize
+    @head = nil
+  end
+
+  def append(value)
+    if @head
+      find_tail.next = Node.new(value)
+    else
+      @head = Node.new(value)
+    end
+  end
+
+  def find_tail
+    node = @head
+    # return node which is like the head if we do not have a next node
+    return node if !node.next
+    return node if !node.next while(node = node.next)
+  end
+end
+
+list = LinkedList.new
+list.append(10)
+list.append(20)
+list.append(30)
+# list.append_after(10, 15)
+# list.append_after(20, 25)
+# list.print
