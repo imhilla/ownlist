f395f0ececd6de261d30dc46003d52e6d8f757ed
imhilla <hillaryodhiambo282@gmail.com>
2022-10-04 19:17:41 +0300

    add descrption

---
+++ ownlist.rb
@@ -0,0 +1,12 @@
+# linked list is a way to store data in a list format
+# data is stored in a node and these nodes are linked to each other in a sequential manner
+# linked list can be more efficient for certain tasks than an array
+# there is no indexing, no random access, you cant reach out for an item in the middle of an array
+# you have to start at the head of the list and follow the links until you find the node you want or until the end of the list
+# removing or adding an item from the middle of a linked list is a lot faster
+
+## operations
+# 1. append(to the end of the list)
+# 2. append_after
+# 3. delete
+# 4. find
